f209fcbd26beb28ef9fd04dbe04ff51cf2e6f04f
waqas <wmunir232@gmail.com>
2024-04-12 15:53:18 +0500

    Diff b/w var and val

--- Mobile Development With Kotlin/kotlin-basics/src/variable.kt
+++ Mobile Development With Kotlin/kotlin-basics/src/variable.kt
@@ -1,9 +1,11 @@
 fun main(){
 
-//    In Kotlin, values are inferred from the initial value which is assign to it. A variable is declared by used val keyword in kotlin
-
+//    In Kotlin, values are inferred from the initial value which is assign to it.
     val name = "Leonard"
     val age = "12"
 
     println("Name is $name & age is $age")
+
+
+//    In kotlin, variables are declared by using var keyword and values ( variables whose values can't be changed like constants are declared by used val )
 }
