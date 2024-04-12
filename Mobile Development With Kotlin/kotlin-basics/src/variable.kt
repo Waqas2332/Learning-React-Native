10a6f8ff3f59d3a26f9e55e61a54bcdc37ffb94d
waqas <wmunir232@gmail.com>
2024-04-12 15:51:10 +0500

    Variables & datatypes in kotlin

---
+++ Mobile Development With Kotlin/kotlin-basics/src/variable.kt
@@ -0,0 +1,9 @@
+fun main(){
+
+//    In Kotlin, values are inferred from the initial value which is assign to it. A variable is declared by used val keyword in kotlin
+
+    val name = "Leonard"
+    val age = "12"
+
+    println("Name is $name & age is $age")
+}
