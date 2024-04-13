d50bbc2e9bf0c9e1eeb0a325df21347e422a50f7
waqas <wmunir232@gmail.com>
2024-04-13 15:48:45 +0500

    Strings in Kotlin

---
+++ Mobile Development With Kotlin/kotlin-basics/src/strings.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var ch = 'a'
+
+//    checking unicode for any character in kotlin
+    println(ch.code)
+
+    val str = "Waqas"
+    println(str.length) // finding string length
+
+    val num = 123;
+    println(num.toString()) // converting anythinng into strings
+}
