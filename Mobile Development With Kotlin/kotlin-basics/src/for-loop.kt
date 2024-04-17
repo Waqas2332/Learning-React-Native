dc371f573f36d5cbbbd31168307d62e84e47ec95
waqas <wmunir232@gmail.com>
2024-04-17 11:30:47 +0500

    for loops

---
+++ Mobile Development With Kotlin/kotlin-basics/src/for-loop.kt
@@ -0,0 +1,16 @@
+fun main(){
+    for(i in 1..5){
+        println(i)
+    }
+
+//    using step
+    for(i in 1..5 step 2){
+        print(i)
+    }
+
+//    looping in reverse order
+    for(i in 5 downTo 1){
+        print(i)
+    }
+
+}
