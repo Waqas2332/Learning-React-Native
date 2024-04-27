3d4484f19dc6074cd14c3ebf4f6a6b9c9b74b2c6
waqas <wmunir232@gmail.com>
2024-04-27 20:31:48 +0500

    constructors and more about classes

--- Mobile Development With Kotlin/kotlin-basics/src/classes.kt
+++ Mobile Development With Kotlin/kotlin-basics/src/classes.kt
@@ -1,9 +1,22 @@
 // All data types are objects in kotlin
 
-class Employee{
+/* class ClassName(
+    constructor definition
+){
+    class definition
+}
+ */
 
+class Employee(
+    val name:String,
+    var position:String
+){
+    var label:String = "New Label"; // if any property is defined in class body, it has to be initialized immediately
 }
 
+
 fun main(){
-    val mark:Employee = Employee() // created an object of employee class
+    val mark:Employee = Employee("Waqas","Manager") // created an object of employee class
+
+    print(mark.label)
 }
